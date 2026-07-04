d30985e2c740cb2d607dc73d499d62eec1e292f4
Saim <majesticpainter79@gmail.com>
2026-07-04 18:53:01 +0500

    fix unused react

--- dashboard/src/components/Menu.js
+++ dashboard/src/components/Menu.js
@@ -1,4 +1,4 @@
-import React , {useState} from "react";
+import {useState} from "react";
 import { Link } from "react-router-dom";
 import { FormatListBulletedOutlined } from "@mui/icons-material";
 
